6b2ac6b87ca7a037d3959a3419bc881abdfcec38
Olli Wang <olliwang@ollix.com>
2018-06-20 22:02:06 +0800

    Reverts `nvgCreateImageFromPixels()`.

--- moui/nanovg_hook.h
+++ moui/nanovg_hook.h
@@ -114,7 +114,6 @@ int nvgCreateImageAtPath(NVGcontext* context, const std::string path,
 // Creates an image for pixels read from `nvgReadPixels()`.
 int nvgCreateImageFromPixels(NVGcontext* context, const int width,
                              const int height, const int image_flags,
-                             const bool flips_image_vertically_on_demand,
                              const unsigned char* data);
 
 // Deletes the passed image and set the image id to -1.
