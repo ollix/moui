f7d5b8c2621ad3e9aa4bb4a4210454732049c5c6
Olli Wang <olliwang@ollix.com>
2022-09-22 11:48:53 +0800

    Updates dependent libraries.

--- cmake/hunter.cmake
+++ cmake/hunter.cmake
@@ -11,5 +11,5 @@ if (ANDROID)
 endif(ANDROID)
 
 include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
-HunterGate(URL "https://github.com/cpp-pm/hunter/archive/v0.23.312.tar.gz"
-           SHA1 "42311324f6b859983d833cffc2151d0b86b8bea4")
+HunterGate(URL "https://github.com/cpp-pm/hunter/archive/v0.24.7.tar.gz"
+           SHA1 "908ddcb223f25fa155315baae64ce0ab48a38bf7")
